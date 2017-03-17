cb469e1ad7f355e86821386a4646295a0f4fefb9
Zhu Li <zhuli19901106@gmail.com>
2017-03-17 15:50:10 +0800

    minor correction

--- 3sum-smaller_1_AC.cpp
+++ 3sum-smaller_1_AC.cpp
@@ -1,44 +1,23 @@
 #include <algorithm>
-#include <utility>
 #include <vector>
-using std::make_pair;
-using std::pair;
 using std::sort;
 using std::vector;
-
-typedef pair<int, int> PII;
-bool comparator(const PII &p1, const PII &p2)
-{
-    if (p1.first != p2.first) {
-        return p1.first < p2.first;
-    }
-    if (p1.second != p2.second) {
-        return p1.second < p2.second;
-    }
-    return false;
-}
 
 class Solution {
 public:
     int threeSumSmaller(vector<int>& nums, int target) {
         auto &a = nums;
         int n = a.size();
+        sort(a.begin(), a.end());
         
-        vector<PII> v;
-        int i;
-        for (i = 0; i < n; ++i) {
-            v.push_back(make_pair(a[i], i));
-        }
-        sort(v.begin(), v.end(), comparator);
-        
-        int j, k;
+        int i, j, k;
         int res = 0;
         int sum;
         for (i = 0; i < n - 2; ++i) {
             k = n - 1;
             for (j = i + 1; j < k && j < n - 1; ++j) {
                 while (j < k) {
-                    sum = v[i].first + v[j].first + v[k].first;
+                    sum = a[i] + a[j] + a[k];
                     if (sum < target) {
                         break;
                     }
@@ -47,7 +26,6 @@ public:
                 res += k - j;
             }
         }
-        v.clear();
         
         return res;
     }
